ba7426409899fa5c6f3d08fb97e87a1a71d061b6
Damian Urbaniak <damianourbaniak@gmail.com>
2022-11-17 12:50:14 +0100

    Initial commit

---
+++ src/test/java/com/DamianUrbaniak/Battleships/BattleshipsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DamianUrbaniak.Battleships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattleshipsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
